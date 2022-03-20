921f7cbab8a9a02fd26a3444f601f61568a128ff
IIMURA Takuji <uirou.j@gmail.com>
2022-03-20 16:08:06 +0900

    ローカライズメッセージ周りを将来的に楽に書き換えられる方向にした

--- DialogEraser/background.js
+++ DialogEraser/background.js
@@ -1,39 +1,87 @@
 chrome.action.onClicked.addListener((tab) => {
-    console.log("clicked", tab);
     chrome.tabs.sendMessage(tab.id, {type: "ToggleAllDialogs"});
 });
 
 function UnDisplayThis(tabID) {
-    console.log("UnDisplayThis called");
     chrome.tabs.sendMessage(tabID, {type: "UnDisplayThis"})
 }
 
-const ContextMenuID_UnDisplayThis = "ContextMenuTitle_UnDisplayThis";
-const rightClickMenuTitleMap_UnDisplayThis = {
-    en: "Hide this",
-    ja: "非表示にする",
-    zh_CN: "隐藏这个",
-    zh_TW: "隱藏這個",
+function AssignDisableThisSite(tabID) {
+    chrome.tabs.sendMessage(tabID, {type: "AssignDisableThisSite"})
 }
+
 const langCode = navigator.language;
-var ContextMenuTitle_UnDisplayThis = rightClickMenuTitleMap_UnDisplayThis[langCode];
-if(ContextMenuTitle_UnDisplayThis.length <= 0){
-    ContextMenuTitle_UnDisplayThis = rightClickMenuTitleMap_UnDisplayThis["en"];
+const DefaultLanguageCode = "en";
+const ContextMenuID_UnDisplayThis = "ContextMenuTitle_UnDisplayThis";
+const ContextMenuID_AssignDisableThisSite = "ContextMenuTitle_AssignDisableThisSite";
+const MESSAGES = {
+    "en": {
+        "ContextMenuTitle_UnDisplayThis": {
+            "message": "Hide this"
+        },
+        "ContextMenuTitle_AssignDisableThisSite": {
+            "message": "Always hide dialogs on this site"
+        }
+    },
+    "ja": {
+        "ContextMenuTitle_UnDisplayThis": {
+            "message": "非表示にする"
+        },
+        "ContextMenuTitle_AssignDisableThisSite": {
+            "message": "常にこのサイトでダイアログを非表示にする"
+        }
+    },
+    "zh_CN": {
+        "ContextMenuTitle_UnDisplayThis": {
+            "message": "隐藏这个"
+        },
+        "ContextMenuTitle_AssignDisableThisSite": {
+            "message": "始终隐藏此站点上的对话框"
+        }
+    },
+    "zh_TW": {
+        "ContextMenuTitle_UnDisplayThis": {
+            "message": "隱藏這個"
+        },
+        "ContextMenuTitle_AssignDisableThisSite": {
+            "message": "始終隱藏此站點上的對話框"
+        }
+    }
+};
+function GetLocalizedString(target){
+    if(chrome.i18n.getMessage){
+        const message = chrome.i18n.getMessage(target);
+        return message;
+    }
+    const message = MESSAGES[navigator.language]?.[target]?.message;
+    if(message){ return message; }
+    return MESSAGES[DefaultLanguageCode]?.[target]?.message;
 }
+
+chrome.contextMenus.create({
+    id: ContextMenuID_UnDisplayThis,
+    title: GetLocalizedString(ContextMenuID_UnDisplayThis),
+    contexts: ["page"],
+    type: "normal",
+}, () => chrome.runtime.lastError);
+
+chrome.contextMenus.create({
+    id: ContextMenuID_AssignDisableThisSite,
+    title: GetLocalizedString(ContextMenuID_AssignDisableThisSite),
+    contexts: ["action"],
+    type: "normal",
+}, () => chrome.runtime.lastError);
 
 chrome.contextMenus.onClicked.addListener((info,tab) => {
     switch(info.menuItemId){
         case ContextMenuID_UnDisplayThis:
             UnDisplayThis(tab.id);
             break;
+        case ContextMenuID_AssignDisableThisSite:
+            AssignDisableThisSite(tab.id);
+            break;
         default:
             console.log("unknown menuItemId", info.menuItemId, info);
             break;
     }
 });
-chrome.contextMenus.create({
-    id: ContextMenuID_UnDisplayThis,
-    title: ContextMenuTitle_UnDisplayThis,
-    contexts: ["page"],
-    type: "normal",
-}, () => chrome.runtime.lastError);
